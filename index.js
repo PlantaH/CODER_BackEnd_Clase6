46b63a5400e23a1080caa7eab3dd25e6e61ba5df
PlantaH <laplantah@gmail.com>
2022-01-12 18:04:58 -0300

    first commit

--- index.js
+++ index.js
@@ -1,26 +1,19 @@
 const express = require('express')
+const contenedor = require("./contenedor.js");
+const file = new contenedor('./productos.txt')
 
-
-const PORT = 8080
+const PORT = 8080 || process.env.PORT;
 const app = express()
  
 app.get('/', function (req, res) {
-    res.send('Express Server Clase-6')
+    res.send('* Express Server Clase-6 *')
 })
 
-app.get('/productos/', function (req, res) {
-    const contenedor = require("./contenedor.js");
-
-    let file = new contenedor('./productos.txt') 
-
+app.get('/productos/', function (req, res) {   
     res.send(file.getAll())
 })
 
-app.get('/productoRandom/', function (req, res) { 
-    const contenedor = require("./contenedor.js");
-
-    let file = new contenedor('./productos.txt') 
-
+app.get('/productoRandom/', function (req, res) {  
     res.send(file.getRandom())
 })
 
